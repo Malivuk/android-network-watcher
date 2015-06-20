ac2c91b9b0405c02b093ba69846f9502994693ff
Malivuk <jl.trolliard@gmail.com>
2015-06-20 13:52:56 +0900

    2d commit

---
+++ NetworkWatcher.java
@@ -0,0 +1,65 @@
+package com.{package};
+
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+
+class NetworkWatcher {
+
+    // Check if bluetooth is enabled
+    public boolean checkBT() {
+
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        return mBluetoothAdapter != null && mBluetoothAdapter.isEnabled();
+    }
+
+
+    // Check if data/wifi is enabled
+    public boolean checkIT(Context context) {
+
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+    }
+
+
+    // Enables bluetooth
+    public void enableBT() {
+
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        bluetoothAdapter.enable();
+    }
+}
+
+
+class BluetoothListener extends BroadcastReceiver {
+
+    MainActivity parentActivity;
+
+    public BluetoothListener(MainActivity parentActivity) {
+        this.parentActivity = parentActivity;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        final String action = intent.getAction();
+
+        if (action.equals(BluetoothAdapter.ACTION_STATE_CHANGED)) {
+            final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR);
+
+            // Send broadcast when BT is ready
+            if(state == BluetoothAdapter.STATE_ON) {
+
+                // Restart service
+                parentActivity.handleService();
+            }
+
+            // Check other states...
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # android-network-watcher
 Check network and Bluetooth states and listen for changes.
+
+```
+@Override
+public void onCreate(Bundle savedInstanceState) {
+
+		// Check network
+		boolean isIT = new NetworkHelper().checkIT( (Context) {CONTEXT});
+
+		// Register BluetoothListener
+		BluetoothListener mReceiver = new BluetoothListener(this);
+		registerReceiver(mReceiver, filter);
+}
+
+
+@Override
+public void onDestroy() {
+    super.onDestroy();
+
+    // Unregister receiver if set
+    try {
+        this.unregisterReceiver(mReceiver);
+    } catch(IllegalArgumentException e) {
+        System.out.println(e);
+    }
+}
+```
